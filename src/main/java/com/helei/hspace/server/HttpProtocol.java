043132d481ae382b67717fe790e290bf50cf7e3e
helei <heleifz@126.com>
2018-03-31 01:02:14 +0800

    update

---
+++ src/main/java/com/helei/hspace/server/HttpProtocol.java
@@ -0,0 +1,13 @@
+package com.helei.hspace.server;
+
+/**
+ * Implementation of http protocol parser 
+ */
+public class HttpProtocol
+{
+    // HTTP Method
+    public enum Method {
+        GET, POST
+    }
+
+}
--- src/main/java/com/helei/hspace/util/Pair.java
+++ src/main/java/com/helei/hspace/util/Pair.java
@@ -2,12 +2,20 @@ package com.helei.hspace.util;
 
 public class Pair<L, R> {
 
-    private final L first;
-    private final R second;
+    private L first;
+    private R second;
 
     public Pair(L first, R second) {
         this.first = first;
         this.second = second;
+    }
+
+    public void setFirst(L val) {
+        this.first = val;
+    }
+
+    public void setSecond(R val) {
+        this.second = val; 
     }
 
     public L getFirst() {
